5d01281fbdec1e2214700ba694f32cbbdc9ade07
L <82750157+Anubhav6205@users.noreply.github.com>
2021-08-17 23:15:27 +0530

    Add files via upload

---
+++ reverse_string.c
@@ -0,0 +1,16 @@
+/*Write a function that reverses a string. The input string is given as an array of characters s.*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<right)
+        {
+            char temp=s[left];
+            s[left++]=s[right];
+            s[right--]=temp;
+        }
+        
+        
+    }
+};
