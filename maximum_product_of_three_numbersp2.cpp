00ec2c388c1a1f756782f469c95520021d4b0109
L <82750157+Anubhav6205@users.noreply.github.com>
2021-08-20 13:16:27 +0530

    Add files via upload

---
+++ maximum_product_of_three_numbersp2.cpp
@@ -0,0 +1,12 @@
+/*Given an integer array nums, find three numbers whose product is maximum and return the maximum product.*/
+
+class Solution {
+public:
+int maximumProduct(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int temp1 = nums[n-1]*nums[n-2]*nums[n-3];
+    int temp2 = nums[0]*nums[1]*nums[n-1];
+    return temp1>temp2?temp1:temp2;
+}
+};
