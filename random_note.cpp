5a5b8eb2dad03601d3be980961413b3a4bc861a0
L <82750157+Anubhav6205@users.noreply.github.com>
2021-08-22 13:04:33 +0530

    Add files via upload

---
+++ random_note.cpp
@@ -0,0 +1,40 @@
+/*Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.*/
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int rr=ransomNote.length();
+    int count=0;
+    for(int i=0;i<ransomNote.length();i++)
+    {
+        label:
+        for(int j=0;j<magazine.length();j++)
+        {
+            if(ransomNote[i]==magazine[j])
+            {
+                magazine.erase(magazine.begin()+j);
+                i++;
+                count++;
+                if(i<ransomNote.length())
+                {
+                    goto label;
+
+                }
+                
+    
+            }
+
+        }
+    }
+    if(count==rr)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+        
+    }
+};
