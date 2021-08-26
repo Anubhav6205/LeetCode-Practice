00f158967506c3de5a8b65e396d3bcf8dd91363a
L <82750157+Anubhav6205@users.noreply.github.com>
2021-08-26 23:19:47 +0530

    Add files via upload

--- random_note.cpp
+++ random_note.cpp
@@ -1,6 +1,7 @@
 /*Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
 
 Each letter in magazine can only be used once in ransomNote.*/
+
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
