8117bb318787a3c081b5dd761e58c9d7feffe2fa
L <82750157+Anubhav6205@users.noreply.github.com>
2021-08-30 04:10:41 +0530

    Add files via upload

---
+++ three_divisors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isThree(int n) {
+            int i,count=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+            if(count>3)
+            {
+                return false;
+            }
+    
+        }
+    }
+        if(count<3)
+        {
+            return false;
+        }
+    return true;
+        
+    }
+};//completed
