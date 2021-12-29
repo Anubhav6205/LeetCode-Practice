7729aa5dfd362875de0a132b5a25c092836d8458
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2021-12-29 08:29:13 -0800

    Add files via upload

---
+++ single_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+}
