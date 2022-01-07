859853e883a16b8a7990483a28ced11aaf929cee
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-01-07 20:32:00 +0530

    Add files via upload

---
+++ add_strings.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string num1="11";
+    string num2="123";
+    int n1=stoi(num1);
+    int n2=stoi(num2);
+    int sum=n1+n2;
+    string s=to_string(sum);
+    cout<<s;
+
+}
