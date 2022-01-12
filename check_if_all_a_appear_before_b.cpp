e43be420b59b37efb8066474eeb8d663c33915ee
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-01-12 22:50:57 +0530

    Add files via upload

---
+++ check_if_all_a_appear_before_b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="bbb";
+    string c=s;
+    sort(s.begin(),s.end());
+    if(c==s)
+    {
+        cout<<"true";
+    }
+    else{
+        cout<<"false";
+    }
+    
+
+
+}
