e3aa01fec77a0de3caeb82e6e406bd95518aa8b4
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-03-19 13:00:06 +0530

    Add files via upload

---
+++ transpose_matrix.cpp
@@ -0,0 +1,17 @@
+/*Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.*/
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+     int m = matrix.size(); //rows
+     int n = matrix[0].size(); //cols
+    vector<vector<int>>ans(n,vector<int>(m));
+    
+    for(int i =0;i<m;i++){
+        for(int j = 0;j<n;j++){
+           ans[j][i] = matrix[i][j];            }
+    }
+    return ans;
+}
+};
