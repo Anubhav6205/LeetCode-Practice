4f82c5633b302455e61902a4d805765b8120f1b7
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-04-19 12:08:31 +0530

    Add files via upload

---
+++ climbing_stairs.cpp
@@ -0,0 +1,30 @@
+/*You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ */
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1)
+        {
+            return 1;
+        }
+        else if(n==2)
+        {
+            return 2;
+        }
+        else 
+        {
+            int n1=1,n2=2,n3;
+            for(int i=2;i<n;i++)
+            {
+                n3=n1+n2;
+                n1=n2;
+                n2=n3;
+            }
+return n3;
+        }
+        
+    }
+};
