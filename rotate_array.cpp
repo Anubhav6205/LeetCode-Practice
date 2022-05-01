b406577c9defac86ccebe53a2d52156b728262a3
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-05-01 10:48:36 +0530

    Add files via upload

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -1,13 +1,35 @@
-/*Given an array, rotate the array to the right by k steps, where k is non-negative.*/  
-
+/*Given an array, rotate the array to the right by k steps, where k is non-negative.*/
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
+            queue<int>q;
+
+    int i;
+    for(i=nums.size()-1; i>=0;i--)
+    {
+        q.push(nums[i]);
+
+    }
+    while(k--)
+    {
+        int r=q.front();
+        cout<<r<<endl;
+        q.pop();
+        q.push(r);
+
+    }
+    i=0;
+
+    while(q.size()!=0)
+    {
+        int r=q.front();
+        nums[i]=r;
+        q.pop();
+        i++;
+
+    }
         reverse(nums.begin(),nums.end());
-        k=k%nums.size();
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        
         
     }
 };
