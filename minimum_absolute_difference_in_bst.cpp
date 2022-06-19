29f44edd1d8b3733204dfeebde378e132d3946ad
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-06-19 11:23:23 +0530

    Add files via upload

---
+++ minimum_absolute_difference_in_bst.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ * 
+ * Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+ */
+class Solution {
+public:
+    
+    void inorder(TreeNode* root,vector<int> &x)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        inorder(root->left,x);
+        x.push_back(root->val);
+        inorder(root->right,x);
+    }
+    int getMinimumDifference(TreeNode* root) {
+                vector<int>x;
+        inorder(root,x);
+        int mdif=INT_MAX;
+        int i;
+        for(i=0;i<x.size()-1;i++)
+        {
+            mdif=min(mdif,x[i+1] - x[i]);
+         
+        }
+        return mdif;
+        
+    }
+};
