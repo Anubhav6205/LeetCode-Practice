d0aae4624668b3da318bacb0d0eeb61d28465913
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-07-10 21:42:00 +0530

    Add files via upload

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -1,47 +1,17 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-            int i=0;
-    int flag=0;
-    int size=nums.size();
-    if(size==1)
+#include <bits/stdc++.h>
+#include <vector>
+#include <map>
+using namespace std;
+int main()
+{
+    vector<int>nums{1,2,3,1};
+    int arr[100]={0};
+    int i;
+    for(i=0;i<nums.size();i++)
     {
-        return false;
+        arr[nums[i]]++;
     }
-    cout<<"size is "<<size;
-    sort(nums.begin(),nums.end());
-    while(i<nums.size())
-    {
-        
-        if(i==0 && nums[i]==nums[i+1])
-        {
-            return true;
 
-        }
+  
 
-        else if(i==nums.size()-1 && nums[i]==nums[i-1])
-        {
-            return true;
-        }
-        else
-        {
-            if(i!=0 && i!=size-1 && nums[i]==nums[i+1] && nums[i]==nums[i-1])
-            {
-                cout<<"i and val is "<<i<<" "<<nums[i];
-                return true;
-        
-
-            }
-             
-        }
-        i++;
-       
-
-       
-    }
-        return false;
-        
-        
-        
-    }
-};//completed
+}
