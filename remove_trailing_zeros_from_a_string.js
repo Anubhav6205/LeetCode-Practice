a236685deffbb5f086a6df1375bca9c5acd5eac2
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2023-06-07 23:55:49 +0530

    Add files via upload

---
+++ remove_trailing_zeros_from_a_string.js
@@ -0,0 +1,11 @@
+var removeTrailingZeros = function(num) {
+    for(let i=num.length-1;i>=0;i--)
+        {
+            if(num[i]!=='0')
+                {
+                    return num.substring(0,i+1);
+                }
+        }
+    return "";
+    
+};
